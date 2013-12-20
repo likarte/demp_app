df6e815cea159a3fd558e13c4509657a68bb972a
karim <likartecom@gmail.com>
2013-12-20 11:48:25 +0000

    Initial commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '78b09e4980053e380d6c6f104e7de2c050b32bf7b618e9fec944e29f8a66c84acace474f13f41759932dc1aace45da196710b5c1726cf47f4a35ad5987ecb763'
---
+++ lotto/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lotto::Application.config.secret_token = '44bafa7cb86abfb5adcbd25fd4d08e1f0a742bca62ffc46660b15d9e1f46ca1a4e186a09ce47f228169ca9ae43c1061a00279957935d54291699ba4769d58017'
